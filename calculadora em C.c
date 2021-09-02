729c89bb7a9b7a6c49914257303ea38f73ed33eb
Luiz Filipe dos Santos Paula <74314665+Luizfii@users.noreply.github.com>
2021-09-02 01:21:45 -0300

    corrigido o problema
    o problema era ocasionado por atribuir valor às variáveis após o usuário já haver atribuído.

--- calculadora em C.c
+++ calculadora em C.c
@@ -4,33 +4,48 @@
 #include <conio.h>
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(void)
- {
-	float a, b,soma, subtracao, divisao,multiplicacao;
-	int p;
-	soma= a+b;
-	subtracao= a-b;
-	divisao= a/b;
-	multiplicacao = a*b;
-	
-	
-	printf ("Bem vindo a minha primeira calculadora em C!\n");
+int main()
+{
+    int a, b, c, d, e, f, ponto;
+
+ 	printf ("Bem vindo a minha primeira calculadora em C!\n");
 	printf ("Neste momento, qual operacao voce precisa?\n 1-Adicao \n 2-Subtracao \n 3-Multiplicacao \n 4-Divisao \n Digite apenas o numero:\n");
-	scanf ("%i", &p);
+	scanf ("%i", &ponto);
+    
+    
+    	printf("digite o primeiro numero:\n");
+	scanf("%i", &a);
+	printf("digite o segundo numero:\n");
+	scanf("%i", &b);
 	
-	if ((p<=0) ||  ( p>= 5))
-	{
-		printf ("esta opção é invalida!");
-		
-		return 0;	}		
-
-	printf ("digite abaixo oprimeiro número de seu calculo: \n");
-	scanf ("%f", &a);
-	printf ("digite abaixo o segundo número de seu calculo: \n");
-	scanf ("%f", &b);
 	
-		printf ("o resultado de sua soma e %f.\n", &soma);
-
-
-		return 0;
+    if ((ponto<1)||(ponto>4))
+    {
+    printf("numero invalido!");
+	return 0;
+	}
+    c            = a + b;
+    d       = a - b;
+    e           = a * b;
+    f             = a / b;
+if (ponto==1){
+   					 printf( "A soma e: %i\n", c );
+   					 return 0;
+	}
+	
+	 		
+	 			if (ponto==2){
+	 				printf( "A subtracao e: %i\n", d );
+	 				return 0;
+					 }
+					 
+				if (ponto==3){
+	 					printf( "O produto e: %i\n", e );
+					 }
+					 
+				if (ponto==4){
+					 		 printf( "A divisão e: %i \n", f );
+						 }					
+    return 0;
 }
+
